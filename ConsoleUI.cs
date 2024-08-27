e0d075bffe4e1554a6f52d6a14cccc4be1c4eaaa
f-guevara <151416828+f-guevara@users.noreply.github.com>
2024-08-27 08:25:54 +0200

    Eliminated "else" in ConsoleUI.ShowMainMenu()

--- ConsoleUI.cs
+++ ConsoleUI.cs
@@ -24,7 +24,7 @@ namespace QuizMaker
                 {
                     return choice;
                 }
-                else
+              
                 {
                     ShowInvalidOptionMessage();
                 }
